a04788a99d819d3d31f7a6318b5ea3889aef677e
Dilan Ozkaynak <dilanjames10@gmail.com>
2020-04-08 19:12:42 -0400

    Updates

---
+++ precache-manifest.51098b8bb28b06da603b266b7847ffeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11587478aeb52f4645f71b2ff08ea755",
+    "url": "/cdm-final2/index.html"
+  },
+  {
+    "revision": "ac2c931873ba3a140e9e",
+    "url": "/cdm-final2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c1d2936f3d79d3bc2eb",
+    "url": "/cdm-final2/static/js/2.bf82200c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cdm-final2/static/js/2.bf82200c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2c931873ba3a140e9e",
+    "url": "/cdm-final2/static/js/main.10ee0ec6.chunk.js"
+  },
+  {
+    "revision": "52458ccab16f56999ba7",
+    "url": "/cdm-final2/static/js/runtime-main.7a636ba8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cdm-final2/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cdm-final/precache-manifest.6341fce9c1035f703a7c8bd0eac9785d.js"
+  "/cdm-final2/precache-manifest.51098b8bb28b06da603b266b7847ffeb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cdm-final/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cdm-final2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
