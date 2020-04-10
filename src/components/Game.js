93553bb04eb0ba6d7a4e6c671235a0dba8c15c45
MDLudwig <46547829+MDLudwig@users.noreply.github.com>
2020-04-10 15:46:38 -0400

    game functionality added

--- src/components/Game.js
+++ src/components/Game.js
@@ -26,22 +26,26 @@ function Square(props) {
         this.state = {
             states: boardStates,
             player1: true,
+            gameOver: false,
+            winner: 'none',
         }
     }
 
-    handleClick(r,c){
-      for(var i = 5; i >= 0; i--){
-        if(this.state.states[i][c] == '0'){
-          var newStates = this.state.states
-          if(this.state.player1){
-            newStates[i][c] = '1'
+    handleClick(c){
+      if(!this.state.gameOver){
+        for(var i = 5; i >= 0; i--){
+          if(this.state.states[i][c] == '0'){
+            var newStates = this.state.states
+            if(this.state.player1){
+              newStates[i][c] = '1'
+            }
+            else {
+              newStates[i][c] = '2'
+            }
+            this.setState({states: newStates})
+            this.setState({player1: !this.state.player1})
+            return
           }
-          else {
-            newStates[i][c] = '2'
-          }
-          this.setState({states: newStates})
-          this.setState({player1: !this.state.player1})
-          return
         }
       }
     }
@@ -62,46 +66,133 @@ function Square(props) {
         squareColor = "yellow-square"
       }
         
-      return <Square value={squareValue} onClick={() => this.handleClick(r,c)} color={squareColor}/>;
+      return <Square value={squareValue} onClick={() => this.handleClick(c)} color={squareColor}/>;
     }
 
     getRow(board ,i){
-        return board[i];
+      return board[i];
+    }
+
+    checkWin(){
+      
+      let currentStates = this.state.states
+      //check horizontal win
+      for(let r = 0; r < 6; r++){
+        for(let c = 0; c < 4; c++){
+          if(currentStates[r][c] == '1' && currentStates[r][c+1] == '1' && currentStates[r][c+2] == '1' && currentStates[r][c+3] =='1'){
+            this.setState({winner: "Player 1"})
+            this.setState({gameOver: true})
+            return 
+          }
+          else if(currentStates[r][c] == '2' && currentStates[r][c+1] == '2' && currentStates[r][c+2] == '2' && currentStates[r][c+3] =='2'){
+            this.setState({winner: "Player 2"})
+            this.setState({gameOver: true})
+            return 
+          }
+        }
+      }
+
+      //check vertical win
+      for(let r = 0; r < 3; r++){
+        for(let c = 0; c < 7; c++){
+          if(currentStates[r][c] == '1' && currentStates[r+1][c] == '1' && currentStates[r+2][c] == '1' && currentStates[r+3][c] =='1'){
+            this.setState({winner: "Player 1"})
+            this.setState({gameOver: true})
+            return 
+          }
+          else if(currentStates[r][c] == '2' && currentStates[r+1][c] == '2' && currentStates[r+2][c] == '2' && currentStates[r+3][c] =='2'){
+            this.setState({winner: "Player 2"})
+            this.setState({gameOver: true})
+            return 
+          }
+        }
+      }
+
+      //check diagonal right 
+      for(let r = 3; r < 6; r++){
+        for(let c = 0; c < 4; c++){
+          if(currentStates[r][c] == '1' && currentStates[r-1][c+1] == '1' && currentStates[r-2][c+2] == '1' && currentStates[r-3][c+3] =='1'){
+            this.setState({winner: "Player 1"})
+            this.setState({gameOver: true})
+            return 
+          }
+          else if(currentStates[r][c] == '2' && currentStates[r-1][c+1] == '2' && currentStates[r-2][c+2] == '2' && currentStates[r-3][c+3] =='2'){
+            this.setState({winner: "Player 2"})
+            this.setState({gameOver: true})
+            return 
+          }
+        }
+      }
+
+      //check diagonal left
+      for(let r = 3; r < 6; r++){
+        for(let c = 3; c < 7; c++){
+          if(currentStates[r][c] == '1' && currentStates[r-1][c-1] == '1' && currentStates[r-2][c-2] == '1' && currentStates[r-3][c-3] =='1'){
+            this.setState({winner: "Player 1"})
+            this.setState({gameOver: true})
+            return 
+          }
+          else if(currentStates[r][c] == '2' && currentStates[r-1][c-1] == '2' && currentStates[r-2][c-2] == '2' && currentStates[r-3][c-3] =='2'){
+            this.setState({winner: "Player 2"})
+            this.setState({gameOver: true})
+            return 
+          }
+        }
+      }
+
+      return
     }
     
     render() {
 
-        let board = []
-        
-        for (let r = 0; r < 6; r++) {
-            let row = [];
-            for (let c = 0; c < 7; c++) { 
-                row.push(this.renderSquare(r, c))
-            }
-            board.push(row);
-        }
+      let board = []
+      
+      for (let r = 0; r < 6; r++) {
+          let row = [];
+          for (let c = 0; c < 7; c++) { 
+              row.push(this.renderSquare(r, c))
+          }
+          board.push(row);
+      }
+
+      if(!this.state.gameOver)
+        this.checkWin()
+      
+        let message = ""
+      if(!this.state.gameOver){
+        message = "It's " + (this.state.player1 ? "player 1's" : "player 2's") + " turn"
+      }
+      else
+        message = this.state.winner + " wins!"
 
       return (
         <div>
-          <div className="board-row">
-            {this.getRow(board, 0)}
+          <div>
+            <div className="board-row">
+              {this.getRow(board, 0)}
+            </div>
+            <div className="board-row">
+              {this.getRow(board, 1)}
+            </div>
+            <div className="board-row">
+              {this.getRow(board, 2)}
+            </div>
+            <div className="board-row">
+              {this.getRow(board, 3)}
+            </div>
+            <div className="board-row">
+              {this.getRow(board, 4)}
+            </div>
+            <div className="board-row">
+              {this.getRow(board, 5)}
+            </div>
           </div>
-          <div className="board-row">
-            {this.getRow(board, 1)}
-          </div>
-          <div className="board-row">
-            {this.getRow(board, 2)}
-          </div>
-          <div className="board-row">
-            {this.getRow(board, 3)}
-          </div>
-          <div className="board-row">
-            {this.getRow(board, 4)}
-          </div>
-          <div className="board-row">
-            {this.getRow(board, 5)}
+
+          <div>
+            {message}
           </div>
         </div>
+
       );
     }
   }
